7a1da93ff2cfaba1e124405e49c467d3b6e5f6f3
NIKITA Vorobyev <73196137+neketskk@users.noreply.github.com>
2022-04-19 16:59:34 +0300

    check2

--- src/fun.cpp
+++ src/fun.cpp
@@ -6,23 +6,23 @@ unsigned int faStr1(const char* str) {
     int count = 0;
     int in = 0;
     int F = 0;
-	while (str[i] != '\0') {
-		if (in == 0 && str[i] != ' ') {
-			if (str[i] > 57 || str[i] < 48) F = 1;
-			in = 1;
-		}
-		else if (in == 1 && str[i] != ' ' && F == 1) {
-			if (str[i] >= 48 && str[i] <= 57) F = 0;
-		}
-		else if (in == 1 && str[i] == ' ') {
-			if (F == 1) count++;
-			F = 0;
-			in = 0;
-		}
-		i++;
-	}
-	if (in == 1 && F == 1) count++;
-	return count;
+    while (str[i] != '\0') {
+        if (in == 0 && str[i] != ' ') {
+            if (str[i] > 57 || str[i] < 48) F = 1;
+                in = 1;
+                }
+                else if (in == 1 && str[i] != ' ' && F == 1) {
+                    if (str[i] >= 48 && str[i] <= 57) F = 0;
+                }
+                else if (in == 1 && str[i] == ' ') {
+                        if (F == 1) count++;
+                        F = 0;
+                        in = 0;
+                }
+                i++;
+        }
+        if (in == 1 && F == 1) count++;
+        return count;
 }
 
 unsigned int faStr2(const char* str) {
