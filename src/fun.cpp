128566bf354f145f8a3613f39624176c6d40771d
NIKITA Vorobyev <73196137+neketskk@users.noreply.github.com>
2022-04-19 17:10:24 +0300

    check3

--- src/fun.cpp
+++ src/fun.cpp
@@ -20,60 +20,60 @@ unsigned int faStr1(const char* str) {
                         in = 0;
                 }
                 i++;
-        }
+    }
         if (in == 1 && F == 1) count++;
         return count;
 }
 
 unsigned int faStr2(const char* str) {
-	int i = 0;
-	int count = 0;
-	int in = 0;
-	int F = 0;
-	while (str[i] != '\0') {
-		if (in == 0 && str[i] >= 65 && str[i] <= 90) {
-			in = 1;
-			F = 1;
-		}
-		else if (in == 1 && str[i] != ' ' && F == 1) {
-			if (str[i] >= 65 && str[i] <= 90) F = 0;
-			if (str[i] > 122 || str[i] < 65) F = 0;
-		}
-		else if (in == 1 && str[i] == ' ') {
-			if (F == 1) count++;
-			F = 0;
-			in = 0;
-		}
-		i++;
+    int i = 0;
+    int count = 0;
+    int in = 0;
+    int F = 0;
+    while (str[i] != '\0') {
+        if (in == 0 && str[i] >= 65 && str[i] <= 90) {
+            in = 1;
+            F = 1;
+        }
+        else if (in == 1 && str[i] != ' ' && F == 1) {
+            if (str[i] >= 65 && str[i] <= 90) F = 0;
+	    if (str[i] > 122 || str[i] < 65) F = 0;
+        }
+	else if (in == 1 && str[i] == ' ') {
+            if (F == 1) count++;
+            F = 0;
+            in = 0;
 	}
+        i++;
+    }
 	if (in == 1 && F == 1) count++;
 	return count;
 }
 
 unsigned int faStr3(const char* str) {
-	int i = 0;
-	int count = 0;
-	int in = 0;
-	int leng = 0;
-	int answer;
-	float sum = 0;
-	while (str[i] != '\0') {
-		if (in == 0 && str[i] != ' ') {
-			count++;
-			in = 1;
-			leng++;
-		}
-		else if (in == 1 && str[i] != ' ') leng++;
-		else if (in == 1 && str[i] == ' '){
-			sum = sum + leng;
-			in = 0;
-			leng = 0;
-		}
-		i++;
-	}
-	if (in == 1) sum = sum + leng;
-	sum = sum / (float)count;
-	answer = sum;
-	if (sum - (float)answer >= 0.5) answer++;
-	return answer;
+    int i = 0;
+    int count = 0;
+    int in = 0;
+    int leng = 0;
+    int answer;
+    float sum = 0;
+    while (str[i] != '\0') {
+        if (in == 0 && str[i] != ' ') {
+            count++;
+            in = 1;
+            leng++;
+        }
+        else if (in == 1 && str[i] != ' ') leng++;
+        else if (in == 1 && str[i] == ' '){
+            sum = sum + leng;
+            in = 0;
+            leng = 0;
+        }
+        i++;
+    }
+    if (in == 1) sum = sum + leng;
+    sum = sum / (float)count;
+    answer = sum;
+    if (sum - (float)answer >= 0.5) answer++;
+    return answer;
 }
